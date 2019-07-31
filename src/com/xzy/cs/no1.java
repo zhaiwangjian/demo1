fbdd8a4d54eb602d584de5c363d87181a2fca54a
zhaiwangjian <zwj950507@163.com>
2019-07-31 21:45:48 +0800

    第一次提交

---
+++ src/com/xzy/cs/no1.java
@@ -0,0 +1,8 @@
+package com.xzy.cs;
+
+public class no1 {
+    public static void main(String []args){
+        System.out.println("你好");
+    }
+
+}
---
+++ src/com/xzy/cs/no2.java
@@ -0,0 +1,5 @@
+package com.xzy.cs;
+
+public class no2 {
+
+}
